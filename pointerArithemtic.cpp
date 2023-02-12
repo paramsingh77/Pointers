69a7be1bd98d79c61df4c2083a00b8cbce25ff0d
Parminder Singh <113473052+paramsingh77@users.noreply.github.com>
2023-02-12 12:35:40 -0600

    Add files via upload

---
+++ pointerArithemtic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    int A[5]={2,3,4,5,7};
+    int *p = A,*q=&A[4];
+    cout<<*q<<endl;
+    cout<<p<<endl;
+    for(int i = 0 ; i < 5 ; i++){
+        cout<<*p<<endl;
+        p++;
+    }
+    cout<<p<<endl;
+    return 0;
+}
